9bc42442418aa7946d01decceacec7861361bb8d
yekaterina-beletskaya <yekaterina.beletskaya@narxoz.kz>
2024-06-02 21:45:41 +0500

    izmenenia2

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -7,14 +7,14 @@ const MenuPage = () => {
 
 
   const items = [
-    { id: 1, name: 'Capuchino', price: '790', description: 'йцу', img:  'capuchino1.jpg'},
-    { id: 2, name: 'Chocolate Milk', price: '820', description: 'йцу', img:  'chocolatemilk1.jpg'},
-    { id: 3, name: 'Correto', price: '1230', description: 'йцу', img:  'correto1.jpg'},
-    { id: 4, name: 'Espresso', price: '550', description: 'йцу', img:  'espresso1.jpg'},
-    { id: 5, name: 'Macchiato', price: '750', description: 'йцу', img:  'macchiato1.jpg'},
-    { id: 6, name: 'Marochono', price: '860', description: 'йцу', img:  'marochino1.jpg'},
-    { id: 7, name: 'Mocha', price: '690', description: 'йцу', img:  'mocha1.jpg'},
-    { id: 8, name: 'Raph', price: '830', description: 'йцу', img:  'raph1.png'},
+    { id: 1, name: 'Capuchino', price: '790', description: 'Классический итальянский кофе, состоящий из эспрессо, горячего молока и воздушной молочной пены. Идеально сбалансированный напиток для утреннего наслаждения.', img:  '/capuchino1.jpg'},
+    { id: 2, name: 'Chocolate Milk', price: '820', description: 'Освежающий и сладкий напиток, приготовленный из холодного молока и шоколадного сиропа или какао. Идеально подходит для любителей шоколада.', img:  '/chocolatemilk1.jpg'},
+    { id: 3, name: 'Correto', price: '1230', description: 'Идеальный выбор для тех, кто ценит насыщенный вкус с легким алкогольным акцентом.', img:  '/correto1.jpg'},
+    { id: 4, name: 'Espresso', price: '550', description: ' Крепкий итальянский кофе с насыщенным вкусом и ароматом, покрытый густой пенкой. Идеален для бодрящего начала дня.', img:  '/espresso1.jpg'},
+    { id: 5, name: 'Macchiato', price: '750', description: 'Идеально сбалансированный напиток для тех, кто предпочитает насыщенный вкус кофе с мягким молочным акцентом.', img:  '/macchiato1.jpg'},
+    { id: 6, name: 'Marochino', price: '860', description: 'Превосходное сочетание крепкого кофе и нежного шоколадного вкуса.', img:  '/marochino1.jpg'},
+    { id: 7, name: 'Mocha', price: '690', description: 'Идеальный выбор для любителей кофе и шоколада с нежным молочным вкусом', img:  '/mocha1.jpg'},
+    { id: 8, name: 'Raph', price: '830', description: ' Нежный кофейный напиток, отличается мягким и кремовым вкусом.', img:  '/raph1.png'},
     { id: 9, name: 'Item 9', price: '5', description: 'йцу', img:  ''},
     { id: 10, name: 'Item 10', price: '5', description: 'йцу', img:  ''},
     { id: 11, name: 'Item 11', price: '5', description: 'йцу', img:  ''},
