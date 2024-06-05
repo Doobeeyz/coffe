cace3b1bf3be2d8fa14b38402870821d4fa14f59
Ilya <ilya.bykov@narxoz.kz>
2024-06-05 10:37:31 +0500

    123

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './css/App.css';
+import upArrow from './images/up-arrow3.png';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import MenuPage from './components/MenuPage';
@@ -21,6 +24,18 @@ function App() {
           <Route path="/detail/:id" element={<CardDetail />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
+      </div>
+      <div id="hero">
+        <div id="hero_text">
+          <h1 className='mt-4'>Мокко<br/> Наяда</h1>
+        </div>
+          <div className="overlay">
+            <img src={upArrow} alt='up-arow' className='up-arrow'/><br/>
+            <div className='overlay_text text-center'>
+              <p>Приветствуем в Мокко Наяда, где каждая чашка - это маленькое произведение искусства. Погрузитесь в атмосферу дружелюбия и ароматов, наслаждайтесь разнообразием напитков и приятными беседами. Ваше утро начинается здесь, вместе с нами!</p>
+              <Link to={`/contact`}  className="button mt-1">Связь с нами</Link>
+            </div>
+          </div>
       </div>
     </Router>
   );
